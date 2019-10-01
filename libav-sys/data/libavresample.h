c3ea118ba173cda363ec5c13b06b47480a357c6f
Yotam Ofek <yotam.ofek@gmail.com>
2019-10-01 23:31:14 +0300

    Compile libavresample.

---
+++ libav-sys/data/libavresample.h
@@ -0,0 +1,1 @@
+#include <libavresample/avresample.h>
--- libav-sys/src/lib.rs
+++ libav-sys/src/lib.rs
@@ -3,18 +3,23 @@
 
 pub mod avcodec;
 pub mod avformat;
+pub mod avresample;
 
 #[cfg(test)]
 mod tests {
     use super::avcodec::avcodec_configuration;
     use super::avformat::avformat_configuration;
+    use super::avresample::avresample_configuration;
     use std::ffi::CStr;
     #[test]
     fn version() {
-        println!("{}{}", unsafe {
-            CStr::from_ptr(avcodec_configuration()).to_string_lossy() },
         unsafe {
-            CStr::from_ptr(avformat_configuration()).to_string_lossy()
-        });
+            println!(
+                "{}{}{}",
+                CStr::from_ptr(avcodec_configuration()).to_string_lossy(),
+                CStr::from_ptr(avformat_configuration()).to_string_lossy(),
+                CStr::from_ptr(avresample_configuration()).to_string_lossy()
+            )
+        };
     }
 }
